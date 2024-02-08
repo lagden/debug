b949fb34566ceea62974fe31da09eb1c6265008b
Thiago Lagden <lagden@gmail.com>
2024-02-08 09:41:02 -0300

    add console

--- src/debug.js
+++ src/debug.js
@@ -7,18 +7,21 @@ const error = debug(`${DEBUG_PREFIX}:error`)
 const warn = debug(`${DEBUG_PREFIX}:warn`)
 const info = debug(`${DEBUG_PREFIX}:info`)
 const log = debug(`${DEBUG_PREFIX}:log`)
+const console = debug(`${DEBUG_PREFIX}:console`)
 
-const colors = [0, 2, 4, 5]
+const colors = [0, 5, 40, 60, 10]
 
 log.color = debug.colors[colors[0]]
 info.color = debug.colors[colors[1]]
 warn.color = debug.colors[colors[2]]
 error.color = debug.colors[colors[3]]
+console.color = debug.colors[colors[4]]
 
 export {
 	log,
 	info,
 	warn,
 	error,
+	console,
 }
 export {default} from 'debug'
--- test/debug.spec.js
+++ test/debug.spec.js
@@ -1,11 +1,12 @@
-import test from 'ava'
+import {test} from 'node:test'
+import assert from 'node:assert/strict'
 import debug from '../src/debug.js'
 import * as _debug from '../src/debug.js'
 
-test('debug', t => {
+test('debug', () => {
 	debug.enable('@tadashi:*')
-	for (const m of ['log', 'info', 'warn', 'error']) {
+	for (const m of ['log', 'info', 'warn', 'error', 'console']) {
 		_debug[m](`___${m}___`)
 	}
-	t.pass()
+	assert.ok(true)
 })
